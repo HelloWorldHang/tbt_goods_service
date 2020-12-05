42140f2695b1180e71e7fdf3dea4ff549540547a
si <si@corp.to8to.com>
2020-12-05 19:09:09 +0800

    Initial commit

---
+++ tbt-goods-server/src/main/java/com/tbt/goods/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.tbt.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
